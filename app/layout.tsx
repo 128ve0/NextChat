cc830b2cbe56f422781e7c21cb4978168fe47290
128ve0 <42941563+128ve0@users.noreply.github.com>
2025-01-14 15:37:49 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,10 +10,10 @@ import { GoogleTagManager, GoogleAnalytics } from "@next/third-parties/google";
 const serverConfig = getServerSideConfig();
 
 export const metadata: Metadata = {
-  title: "NextChat",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "LogicJ-Chat",
+  description: "ChatGPT Chat Bot.",
   appleWebApp: {
-    title: "NextChat",
+    title: "LogicJ-Chat",
     statusBarStyle: "default",
   },
 };
